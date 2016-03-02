b8472a340bf8830d84003ae60bba0c5f332e5adc
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-03-03 07:55:28 +0900

    first commit

---
+++ app/src/main/java/me/rei_m/statepatternsample/MainActivity.java
@@ -0,0 +1,15 @@
+package me.rei_m.statepatternsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
