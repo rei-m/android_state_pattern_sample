74582441e2b27be804875461aef01c70a8b76df5
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-03-04 08:14:48 +0900

    Fragmentを追加

--- app/src/main/java/me/rei_m/statepatternsample/MainActivity.java
+++ app/src/main/java/me/rei_m/statepatternsample/MainActivity.java
@@ -1,7 +1,9 @@
 package me.rei_m.statepatternsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.rei_m.statepatternsample.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.content, MainFragment.newInstance())
+                    .commit();
+        }
     }
 }
